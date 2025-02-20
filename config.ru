f81f594f128925ae22c7ab72b9bd310b98642c00
William <wguiraud@protonmail.com>
2025-02-20 15:36:23 -0800

    refactoring to disable session with env var, applying ls styles to the new list template, refactoring new list template, fixing css link typo

--- config.ru
+++ config.ru
@@ -1,10 +1,11 @@
 require_relative './todo'
 
-use Rack::Session::Cookie, {
-  secret: ENV['SESSION_SECRET'],
-  key: 'rack.session',
-  path: '/',
-  #expire_after: 14400 # Session expiration time in seconds (optional)
-}
+#use Rack::Session::Cookie, {
+#  secret: ENV['SESSION_SECRET'],
+#  key: 'rack.session',
+#  path: '/',
+#  same_site: :lax  # or :strict, :none depending on your needs
+#  #expire_after: 14400 # Session expiration time in seconds (optional)
+#}
 
 run Sinatra::Application
--- todo.rb
+++ todo.rb
@@ -2,15 +2,15 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "erubi"
 
+configure do
+  enable :sessions
+  #set :session_secret, ENV['SESSION_SECRET']
+  set :session_secret, SecureRandom.hex(32)
+end
+
 before do
   session[:lists] ||= []
 end
-
-configure do
-  enable :sessions
-  set :session_secret, ENV['SESSION_SECRET']
-end
-
 
 get "/" do
   redirect "/lists"
@@ -20,4 +20,13 @@ get "/lists" do
   lists = session[:lists]
 
   erb :lists, locals: { lists: lists}
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] << { name: params[:list_name], todos: [] }
+  redirect "/lists"
 end
