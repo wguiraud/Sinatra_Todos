744890fd11f583b8b25c898e07e163e999bfb04d
William <wguiraud@protonmail.com>
2025-02-24 10:45:20 -0800

    naming things better

--- todo.rb
+++ todo.rb
@@ -12,13 +12,30 @@ before do
   session[:lists] ||= []
 end
 
-def used_name?(name)
+def used_list_name?(name)
   session[:lists].any? { |list| list[:name] == name }
 end
 
-def invalid_list_name?(list_name)
-  valid_name = /^[a-zA-Z0-9_]+ ?[a-zA-Z0-9_]+$/
-  !(list_name.match?(valid_name) && (1..100).include?(list_name.length))
+def invalid_character?(name)
+  valid_characters = /^[a-zA-Z0-9_]+ ?[a-zA-Z0-9_]+$/
+  !name.match?(valid_characters)
+end
+
+def invalid_length?(name)
+  !(1..100).include?(name.size)
+end
+
+# using name instead of list_name as a parameter name makes the method
+# more
+# generic. it can be reused in another context if needed
+def error_for_list_name(name)
+  if used_list_name?(name)
+  "The list name must be unique"
+  elsif invalid_character?(name)
+    "The list name must contain valid alphanumeric characters"
+  elsif invalid_length?(name)
+    "The list name must be between 1 and 100 characters"
+  end
 end
 
 def remove_white_spaces(list_name)
@@ -42,11 +59,9 @@ end
 post "/lists" do
   list_name = remove_white_spaces(params[:list_name])
 
-  if invalid_list_name?(list_name)
-    session[:error] = "The list name must be between 1 and 100 characters"
-    erb :new_list
-  elsif used_name?(list_name)
-    session[:error] = "The list name must be unique"
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
     erb :new_list
   else
     session[:lists] << { name: list_name, todos: [] }
