9da45c84f3baff49c51eb7d363c4482e3179dcc1
William <wguiraud@protonmail.com>
2025-03-03 11:47:13 -0800

    experimenting with a procfile

---
+++ config/puma.rb
@@ -0,0 +1,6 @@
+# ruby
+threads_count = ENV.fetch("MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RACK_ENV") { "development" }
